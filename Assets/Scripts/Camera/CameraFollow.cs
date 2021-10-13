b24fd79cc6a0838293c859a382a7ee6df1b27989
Bach Nguyen <nlebachnlb.develop@gmail.com>
2021-10-13 15:32:18 +0700

    Fix glitching following camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed;
     public Vector3 offset;
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
