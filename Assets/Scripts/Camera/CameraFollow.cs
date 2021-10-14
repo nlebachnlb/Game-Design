3f0b7604780b64e026f5af82cbca5409ced13329
Bach Nguyen <nlebachnlb.develop@gmail.com>
2021-10-14 23:31:10 +0700

    Camera boundaries (no boundary transition) :

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,15 +1,42 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public struct Boundary
+{
+    public Vector2 min, max;
+}
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed;
-    public Vector3 offset;
+    public Vector2 offset;
 
-    void FixedUpdate()
+    [Range(0, 100)]
+    public float cameraDistance;
+
+    [Header("Camera Boundaries")]
+    public List<Boundary> boundaries;
+    public int currentBoundaryIndex;
+
+    private void Start()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        currentBoundaryIndex = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 desiredPosition = (Vector2)target.position + offset;
+        Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, smoothSpeed);
+        var currBoundary = boundaries[currentBoundaryIndex];
+        Vector3 finalPosition = 
+            new Vector3
+            (
+                Mathf.Clamp(smoothedPosition.x, currBoundary.min.x, currBoundary.max.x),
+                Mathf.Clamp(smoothedPosition.y, currBoundary.min.y, currBoundary.max.y),
+                -cameraDistance
+            );
+        transform.position = finalPosition;
     }
 }
