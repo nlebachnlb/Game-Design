2bad118872950f19d892503e4399b97638af1e1e
TrDayken <18520542@gm.uit.edu.vn>
2021-10-18 21:23:14 +0700

    Change property to Pascal case

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,17 +6,17 @@ using UnityEngine;
 
 public class PlayerController : Controller<GamePlayApplication>
 {
-    public PlayerModel playerModel { get; private set; }
-    public PlayerView playerView { get; private set; }
+    public PlayerModel PlayerModel { get; private set; }
+    public PlayerView PlayerView { get; private set; }
     public float horizontalInputDirection { get; private set; }
     public float verticalInputDicretion { get; private set; }
 
     private void Start()
     {
-        playerModel = app.model.GetComponentInChildren<PlayerModel>();
-        playerView = app.view.GetComponentInChildren<PlayerView>();
+        PlayerModel = app.model.GetComponentInChildren<PlayerModel>();
+        PlayerView = app.view.GetComponentInChildren<PlayerView>();
 
-        var stateBehaviours = playerView.animator.GetBehaviours<StateMachineBehaviourExt>();
+        var stateBehaviours = PlayerView.Animator.GetBehaviours<StateMachineBehaviourExt>();
         foreach (var state in stateBehaviours)
         {
             state.InitController(this);
@@ -25,66 +25,66 @@ public class PlayerController : Controller<GamePlayApplication>
 
     public void Move()
     {
-        if (playerModel.IsDashing) return;
-        float moveBy = horizontalInputDirection * playerModel.moveSpeed * Time.fixedDeltaTime;
-        playerView.rb.velocity = new Vector2(moveBy, playerView.rb.velocity.y);
+        if (PlayerModel.IsDashing) return;
+        float moveBy = horizontalInputDirection * PlayerModel.moveSpeed * Time.fixedDeltaTime;
+        PlayerView.RB.velocity = new Vector2(moveBy, PlayerView.RB.velocity.y);
     }
 
     private void Jump()
     {
-        if (Input.GetKey(playerModel.jumpKey) && playerModel.isJumping == true)
+        if (Input.GetKey(PlayerModel.jumpKey) && PlayerModel.isJumping == true)
         {
-            if (playerModel.jumpTimeCounter > 0)
+            if (PlayerModel.jumpTimeCounter > 0)
             {
-                playerView.rb.velocity = Vector2.up * playerModel.jumpForce + new Vector2(playerView.rb.velocity.x, 0);
-                playerModel.jumpTimeCounter -= Time.deltaTime;
+                PlayerView.RB.velocity = Vector2.up * PlayerModel.jumpForce + new Vector2(PlayerView.RB.velocity.x, 0);
+                PlayerModel.jumpTimeCounter -= Time.deltaTime;
             }
             else
             {
-                playerModel.isJumping = false;
+                PlayerModel.isJumping = false;
             }
         }
 
-        if (Input.GetKeyUp(playerModel.jumpKey))
+        if (Input.GetKeyUp(PlayerModel.jumpKey))
         {
-            playerModel.isJumping = false;
+            PlayerModel.isJumping = false;
         }
     }
 
     private void Dash()
     {
-        if (playerModel.IsDashing)
+        if (PlayerModel.IsDashing)
         {
-            switch (playerModel.dashPhase)
+            switch (PlayerModel.dashPhase)
             {
                 case 1:
                     {
-                        if (playerModel.dashTimer > playerModel.maxDashTime * 0.1f)
+                        if (PlayerModel.dashTimer > PlayerModel.maxDashTime * 0.1f)
                         {
-                            playerView.rb.velocity = playerModel.dashDirection.normalized * playerModel.dashSpeed * Time.fixedDeltaTime;
-                            playerModel.dashTimer -= Time.deltaTime;
+                            PlayerView.RB.velocity = PlayerModel.dashDirection.normalized * PlayerModel.dashSpeed * Time.fixedDeltaTime;
+                            PlayerModel.dashTimer -= Time.deltaTime;
                         }
                         else
                         {
-                            // playerView.rb.velocity = Vector2.zero;
-                            var vel = playerView.rb.velocity;
+                            // PlayerView.RB.velocity = Vector2.zero;
+                            var vel = PlayerView.RB.velocity;
                             vel.y *= 0.5f;
                             vel.x *= 0.5f;
-                            playerView.rb.velocity = vel;
-                            playerView.rb.gravityScale = 5f;
-                            playerModel.dashPhase = 2;
-                            playerView.StopDash();
+                            PlayerView.RB.velocity = vel;
+                            PlayerView.RB.gravityScale = 5f;
+                            PlayerModel.dashPhase = 2;
+                            PlayerView.StopDash();
                         }
                     }
                     break;
                 case 2:
                     {
-                        var epsilon = playerModel.isWalking ? playerModel.moveSpeed * 0.8f * Time.fixedDeltaTime : playerModel.dashFriction * Time.fixedDeltaTime;
-                        playerView.rb.velocity += -playerModel.dashDirection.normalized * playerModel.dashFriction * Time.fixedDeltaTime;
+                        var epsilon = PlayerModel.isWalking ? PlayerModel.moveSpeed * 0.8f * Time.fixedDeltaTime : PlayerModel.dashFriction * Time.fixedDeltaTime;
+                        PlayerView.RB.velocity += -PlayerModel.dashDirection.normalized * PlayerModel.dashFriction * Time.fixedDeltaTime;
 
-                        if (playerView.rb.velocity.magnitude <= epsilon || playerView.rb.velocity.normalized == -playerModel.dashDirection.normalized)
+                        if (PlayerView.RB.velocity.magnitude <= epsilon || PlayerView.RB.velocity.normalized == -PlayerModel.dashDirection.normalized)
                         {
-                            playerModel.dashPhase = 0;
+                            PlayerModel.dashPhase = 0;
                         }
                     }
                     break;
@@ -92,8 +92,8 @@ public class PlayerController : Controller<GamePlayApplication>
         }
         else
         {
-            if (playerModel.isGrounded && playerView.rb.velocity.y >= 0)
-                playerModel.CurrentNumberofDash = 0;
+            if (PlayerModel.isGrounded && PlayerView.RB.velocity.y >= 0)
+                PlayerModel.CurrentNumberofDash = 0;
         }
 
     }
@@ -105,62 +105,63 @@ public class PlayerController : Controller<GamePlayApplication>
 
     private void Update()
     {
-        playerModel.isGrounded = 
-            Physics2D.OverlapCircle(playerModel.feetPosition.position, playerModel.checkRadius, playerModel.whatIsGround) &&
-            playerView.rb.velocity.y <= 0f;
 
-        if (playerModel.isGrounded && Input.GetKeyDown(playerModel.jumpKey))
+
+        if (PlayerModel.isGrounded && Input.GetKeyDown(PlayerModel.jumpKey))
         {
             // Jump down a platform
             if (verticalInputDicretion < 0)
             {
-                playerView.collisionHandler.DisablePlatform();
+                PlayerView.CollisionHandler.DisablePlatform();
             }
             else
             {
-                playerModel.isJumping = true;
-                playerModel.ResetJumpTimeCounter();
-                playerView.rb.velocity = Vector2.up * playerModel.jumpForce + new Vector2(playerView.rb.velocity.x, 0);
+                PlayerModel.isJumping = true;
+                PlayerModel.ResetJumpTimeCounter();
+                PlayerView.RB.velocity = Vector2.up * PlayerModel.jumpForce + new Vector2(PlayerView.RB.velocity.x, 0);
             }
         }
 
-        if (((Input.GetKeyDown(playerModel.dashKey)) || (Input.GetMouseButtonDown(1)))
-            && (playerModel.CurrentNumberofDash < playerModel.MaxNumberOfDash))
+        if (((Input.GetKeyDown(PlayerModel.dashKey)) || (Input.GetMouseButtonDown(1)))
+            && (PlayerModel.CurrentNumberofDash < PlayerModel.MaxNumberOfDash))
         {
-            playerModel.dashDirection =
+            PlayerModel.dashDirection =
                 horizontalInputDirection != 0 || verticalInputDicretion != 0 ?
                 new Vector2(horizontalInputDirection, verticalInputDicretion) :
-                Vector2.right * playerModel.facing;
+                Vector2.right * PlayerModel.facing;
 
-            playerModel.dashPhase = 1;
-            playerModel.dashTimer = playerModel.maxDashTime;
+            PlayerModel.dashPhase = 1;
+            PlayerModel.dashTimer = PlayerModel.maxDashTime;
 
-            playerView.rb.gravityScale = 0f;
+            PlayerView.RB.gravityScale = 0f;
 
-            playerModel.CurrentNumberofDash++;
-            Debug.Log("INFO" + playerModel.isGrounded);
-            Debug.Log("[INFO]" + playerModel.CurrentNumberofDash);
+            PlayerModel.CurrentNumberofDash++;
+            Debug.Log("INFO" + PlayerModel.isGrounded);
+            Debug.Log("[INFO]" + PlayerModel.CurrentNumberofDash);
 
-            playerView.PlayDash(playerModel.dashDirection);
+            PlayerView.PlayDash(PlayerModel.dashDirection);
         }
     }
 
     private void FixedUpdate()
     {
+        PlayerModel.isGrounded =
+    Physics2D.OverlapCircle(PlayerModel.feetPosition.position, PlayerModel.checkRadius, PlayerModel.whatIsGround) &&
+    PlayerView.RB.velocity.y <= 0f;
         UpdateLoop();
     }
 
     public void UpdateAnimator()
     {
-        playerView.animator.SetBool("isOnGround", playerModel.isGrounded);
-        playerView.animator.SetBool("isDashing", playerModel.IsDashing);
-        playerView.animator.SetBool("isWalking", playerModel.isWalking);
-        playerView.animator.SetFloat("yVelocity", playerView.rb.velocity.y);
+        PlayerView.Animator.SetBool("isOnGround", PlayerModel.isGrounded);
+        PlayerView.Animator.SetBool("isDashing", PlayerModel.IsDashing);
+        PlayerView.Animator.SetBool("isWalking", PlayerModel.isWalking);
+        PlayerView.Animator.SetFloat("yVelocity", PlayerView.RB.velocity.y);
 
         if (horizontalInputDirection != 0)
         {
-            playerModel.facing = (int)Mathf.Sign(horizontalInputDirection);
-            playerView.visual.flipX = playerModel.facing < 0;
+            PlayerModel.facing = (int)Mathf.Sign(horizontalInputDirection);
+            PlayerView.Visual.flipX = PlayerModel.facing < 0;
         }
     }
 
@@ -168,7 +169,7 @@ public class PlayerController : Controller<GamePlayApplication>
     {
         horizontalInputDirection = Input.GetAxis("Horizontal");
         verticalInputDicretion = Input.GetAxisRaw("Vertical");
-        playerModel.isWalking = horizontalInputDirection != 0;
+        PlayerModel.isWalking = horizontalInputDirection != 0;
         Move();
         Jump();
         Dash();
@@ -176,12 +177,12 @@ public class PlayerController : Controller<GamePlayApplication>
 
     public void SpawnAt(Vector2 position, bool reborn = true)
     {
-        playerView.transform.position = position;
+        PlayerView.transform.position = position;
     }
 
     public void ResetStat()
     {
-        playerView.rb.velocity = Vector2.zero; 
+        PlayerView.RB.velocity = Vector2.zero; 
     }
 
     #region Event callback
