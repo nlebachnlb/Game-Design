417214fd8aea20364d92ad963cbbf53ff2241fd8
nlebachnlb <nlebachnlb.develop@gmail.com>
2021-10-23 18:51:08 +0700

    New tutorial + Camera trigger

--- Assets/Scripts/Source/Camera/CameraFollow.cs
+++ Assets/Scripts/Source/Camera/CameraFollow.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using System.Collections;
 
 [System.Serializable]
 public struct Boundary
@@ -9,8 +11,15 @@ public struct Boundary
 
 public class CameraFollow : MonoBehaviour
 {
+    public enum CameraState
+    {
+        Normal = 0,
+        Transition = 1
+    }
+
     public Transform target;
     public float smoothSpeed;
+    public float transitionDurationSeconds;
     public Vector2 offset;
 
     [Range(0, 100)]
@@ -18,25 +27,70 @@ public class CameraFollow : MonoBehaviour
 
     [Header("Camera Boundaries")]
     public List<Boundary> boundaries;
-    public int currentBoundaryIndex;
+
+    private int currentBoundaryIndex;
+    private CameraState state;
+    private Vector3 smoothedPosition;
+
+    public void ChangeBoundary(int index)
+    {
+        if (index < 0 || index >= boundaries.Count)
+        {
+            Debug.LogError("[CameraFollow] Handled error: boundary not exist");
+            return;
+        }
+
+        currentBoundaryIndex = index;
+        StartCoroutine(Transition());
+    }
+
+    private IEnumerator Transition()
+    {
+        state = CameraState.Transition;
+
+        Vector2 desiredPosition = (Vector2)target.position + offset;
+        var currBoundary = boundaries[currentBoundaryIndex];
+        Vector3 finalPosition =
+            new Vector3
+            (
+                Mathf.Clamp(desiredPosition.x, currBoundary.min.x, currBoundary.max.x),
+                Mathf.Clamp(desiredPosition.y, currBoundary.min.y, currBoundary.max.y),
+                -cameraDistance
+            );
+
+        transform.DOMove(finalPosition, transitionDurationSeconds).SetEase(Ease.InOutSine);
+
+        yield return new WaitForSecondsRealtime(transitionDurationSeconds);
+
+        state = CameraState.Normal;
+    }
 
     private void Start()
     {
         currentBoundaryIndex = 0;
+        state = CameraState.Normal;
     }
 
     private void FixedUpdate()
     {
-        Vector2 desiredPosition = (Vector2)target.position + offset;
-        Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, smoothSpeed);
-        var currBoundary = boundaries[currentBoundaryIndex];
-        Vector3 finalPosition = 
-            new Vector3
-            (
-                Mathf.Clamp(smoothedPosition.x, currBoundary.min.x, currBoundary.max.x),
-                Mathf.Clamp(smoothedPosition.y, currBoundary.min.y, currBoundary.max.y),
-                -cameraDistance
-            );
-        transform.position = finalPosition;
+        switch (state)
+        {
+            case CameraState.Normal:
+                {
+                    Vector2 desiredPosition = (Vector2)target.position + offset;
+                    var currBoundary = boundaries[currentBoundaryIndex];
+                    Vector3 finalPosition =
+                        new Vector3
+                        (
+                            Mathf.Clamp(desiredPosition.x, currBoundary.min.x, currBoundary.max.x),
+                            Mathf.Clamp(desiredPosition.y, currBoundary.min.y, currBoundary.max.y),
+                            -cameraDistance
+                        );
+
+                    smoothedPosition = Vector3.Lerp(transform.position, finalPosition, smoothSpeed * Time.fixedDeltaTime);
+                    transform.position = smoothedPosition;
+                }
+                break;
+        }
     }
 }
