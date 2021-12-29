e39c4a9e8d7e874bab894be1149757d14c15f9a9
Bach Nguyen <nlebachnlb.develop@gmail.com>
2021-12-29 10:15:25 +0700

    Saw mechanics

---
+++ Assets/Scripts/Source/Utils/LinePatternMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LinePatternMove : MonoBehaviour
+{
+    public float roundtripTime;
+    public float idleTime;
+    public List<Transform> points;
+    public Ease easing;
+    public bool reverseOnEnd;
+
+    private IEnumerator Start()
+    {
+        int i = 1;
+        int dir = 1;
+        transform.position = points[0].position;
+        while (true)
+        {
+            transform.DOMove(points[i].position, roundtripTime).SetEase(easing);
+            i += dir;
+            if (reverseOnEnd)
+            {
+                if (i >= points.Count || i < 0)
+                {
+                    dir *= -1;
+                    i += dir;
+                }
+            }
+            else
+                if (i >= points.Count) i = 0;
+
+            yield return new WaitForSeconds(roundtripTime + idleTime);
+        }
+    }
+}
