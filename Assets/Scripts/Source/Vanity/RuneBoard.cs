9eaa2071d9bea8e81e9ad73634c3f55cab6c4233
Bach Nguyen <nlebachnlb.develop@gmail.com>
2021-12-30 16:38:27 +0700

    Skill obtaining vfx

--- Assets/Scripts/Source/Vanity/RuneBoard.cs
+++ Assets/Scripts/Source/Vanity/RuneBoard.cs
@@ -1,10 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RuneBoard : BaseTrigger
 {
     public List<Rune> RuneList;
+
+    [SerializeField]
+    private ParticleSystem liberation, lastLiberation;
+    [SerializeField]
+    private SpriteRenderer highlight;
+
+    private bool canGetSkill = false;
+
+    public void PlaySkillObtainFX()
+    {
+        StartCoroutine(SkillObtaining());
+    }
+
+    private IEnumerator SkillObtaining()
+    {
+        GetComponent<Animator>().SetTrigger("Highlight");
+        yield return new WaitForSeconds(1);
+        app.view.CameraShakeFX.Shake(new Vector2(1f, 0f), 5f, 0.25f);
+        liberation.Play();
+        yield return new WaitForSeconds(5);
+        liberation.Stop();
+        yield return new WaitForSeconds(3);
+        app.view.CameraShakeFX.Shake(new Vector2(1f, 0f), 5f, 0.5f);
+        lastLiberation.Play();
+    }
+
+    private void Update()
+    {
+        if (canGetSkill)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PlaySkillObtainFX();
+                canGetSkill = false;
+                GetComponent<Collider2D>().enabled = false;
+            }
+        }
+    }
 
     public override void OnPlayerEnter(PlayerController player)
     {
@@ -41,6 +80,12 @@ public class RuneBoard : BaseTrigger
 
     private void completeRuneBoardAction()
     {
-        Debug.Log("fuck yeah"); 
+        Debug.Log("fuck yeah");
+        canGetSkill = true;
+    }
+
+    private void Start()
+    {
+        //PlaySkillObtainFX();
     }
 }
